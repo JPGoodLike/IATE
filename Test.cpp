29edb32e3378b5268d5daba8200cb9da09748e17
Mikhail <caaalm3@gmail.com>
2020-05-06 13:20:15 +0300

    Doubly linked list and cyclic DL list are added

--- Test.cpp
+++ Test.cpp
@@ -15,12 +15,36 @@ void PrintList(LList<int> list) {
     cout << endl;
 }
 
+void PrintList(DLList<int> list) {
+    if (list.Size() == 0) {
+        cout << "The list is empty\n";
+        return;
+    }
+    for (auto cNode = list.Root(); cNode != nullptr; cNode = cNode->Next()) {
+        cout << cNode->Get() << " ";
+    }
+    cout << endl;
+}
+
+void PrintList(CDLList<int> list) {
+    if (list.Size() == 0) {
+        cout << "The list is empty\n";
+        return;
+    }
+    for (auto cNode = list.Root(); true; cNode = cNode->Next()) {
+        cout << cNode->Get() << " ";
+        if (cNode->Next() == list.Root())
+            break;
+    }
+    cout << endl;
+}
+
 template <typename T>
 void Print(const T& value) {
     cout << value << "\n";
 }
 
-int main() {
+void LListTest() {
     LList<int> list;
     list.Add(5);
     list.Add(3);
@@ -53,5 +77,91 @@ int main() {
         PrintList(list2);
     }
     PrintList(list);
+}
+
+void DLListTest() {
+    DLList<int> list;
+    list.AddFront(5);
+    list.AddFront(3);
+    PrintList(list);
+    list.Remove(5);
+    PrintList(list);
+    list.RemoveByIndex(0);
+    PrintList(list);
+    list.AddFront(7); list.AddFront(9); list.AddFront(3);
+    {
+        DLList<int> list2;
+        list2 = list;
+        PrintList(list);
+        PrintList(list2);
+        auto node1 = list.FindFront(9); 
+        Print(node1->Get());
+        list.Remove(list.Root());
+        list2.RemoveByIndex(list2.Size()-1);
+        PrintList(list);
+        PrintList(list2);
+        list.RemoveByIndex(0);
+        list2.Remove(list2.Get(1));
+        PrintList(list);
+        PrintList(list2);
+
+        list.InsertAfter(8, list.FindBack(3));
+        list.InsertAfter(5, list.Size()-1);
+        list2.InsertAfter(57, 0);
+        PrintList(list);
+        PrintList(list2);
+    }
+    PrintList(list);
+    cout << "Extra Test^:\n";
+    list.AddBack(11);
+    auto node = list.Get(1);
+    list.InsertBefore(list.Get(0)->Get(), node);
+    list.Remove(node);
+    PrintList(list);
+}
+
+void CDLListTest() {
+    CDLList<int> list;
+    list.AddFront(5);
+    list.AddFront(3);
+    PrintList(list);
+    list.Remove(5);
+    PrintList(list);
+    list.RemoveByIndex(0);
+    PrintList(list);
+    list.AddFront(7); list.AddFront(9); list.AddFront(3);
+    {
+        CDLList<int> list2;
+        list2 = list;
+        PrintList(list);
+        PrintList(list2);
+        auto node1 = list.FindFront(9); 
+        Print(node1->Get());
+        list.Remove(list.Root());
+        list2.RemoveByIndex(list2.Size()-1);
+        PrintList(list);
+        PrintList(list2);
+        list.RemoveByIndex(0);
+        list2.Remove(list2.Get(1));
+        PrintList(list);
+        PrintList(list2);
+
+        list.InsertAfter(8, list.FindBack(3));
+        list.InsertAfter(5, list.Size()-1);
+        list2.InsertAfter(57, 0);
+        PrintList(list);
+        PrintList(list2);
+    }
+    PrintList(list);
+    cout << "Extra Test^:\n";
+    list.AddBack(11);
+    auto node = list.Get(1);
+    list.InsertBefore(list.Get(0)->Get(), node);
+    list.Remove(node);
+    PrintList(list);
+}
+
+int main() {
+    CDLListTest();
 
 }
